825f0b4e49ab84d1c62ed403ba17529e6d7b5979
lakshaydulani <lakshaydulani@gmail.com>
2016-01-29 13:14:44 +0530

    Widget unit test case, Mobile list page issue fix

--- widget/app.services.js
+++ widget/app.services.js
@@ -8,7 +8,7 @@
                 return buildfire;
             };
         }])
-        .factory('Location', [function () {
+        /*.factory('Location', [function () {
             var _location = location;
             return {
                 go: function (path) {
@@ -18,7 +18,7 @@
                     _location.href = _location.href.substr(0, _location.href.indexOf('#'));
                 }
             };
-        }])
+        }])*/
         .service('soundCloudAPI', ['$http', '$q', function ($http, $q) {
             var that = this;
 
@@ -67,127 +67,6 @@
                     }
                     else {
                         return deferred.resolve(result);
-                    }
-                });
-                return deferred.promise;
-            };
-            DB.prototype.getById = function (id) {
-                var that = this;
-                var deferred = $q.defer();
-                Buildfire.datastore.getById(id, that._tagName, function (err, result) {
-                    if (err) {
-                        return deferred.reject(err);
-                    }
-                    else if (result && result.data) {
-                        return deferred.resolve(result);
-                    } else {
-                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
-                    }
-                });
-                return deferred.promise;
-            };
-            DB.prototype.insert = function (items) {
-                var that = this;
-                var deferred = $q.defer();
-                if (typeof items == 'undefined') {
-                    return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
-                }
-                if (Array.isArray(items)) {
-                    Buildfire.datastore.bulkInsert(items, that._tagName, function (err, result) {
-                        if (err) {
-                            return deferred.reject(err);
-                        }
-                        else if (result) {
-                            return deferred.resolve(result);
-                        } else {
-                            return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
-                        }
-                    });
-                } else {
-                    Buildfire.datastore.insert(items, that._tagName, false, function (err, result) {
-                        if (err) {
-                            return deferred.reject(err);
-                        }
-                        else if (result) {
-                            return deferred.resolve(result);
-                        } else {
-                            return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
-                        }
-                    });
-                }
-                return deferred.promise;
-            };
-            DB.prototype.find = function (options) {
-                var that = this;
-                var deferred = $q.defer();
-                if (typeof options == 'undefined') {
-                    return deferred.reject(new Error(MESSAGES.ERROR.OPTION_REQUIRES));
-                }
-                Buildfire.datastore.search(options, that._tagName, function (err, result) {
-                    if (err) {
-                        return deferred.reject(err);
-                    }
-                    else if (result) {
-                        return deferred.resolve(result);
-                    } else {
-                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
-                    }
-                });
-                return deferred.promise;
-            };
-            DB.prototype.update = function (id, item) {
-                var that = this;
-                var deferred = $q.defer();
-                if (typeof id == 'undefined') {
-                    return deferred.reject(new Error(MESSAGES.ERROR.ID_NOT_DEFINED));
-                }
-                if (typeof item == 'undefined') {
-                    return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
-                }
-                Buildfire.datastore.update(id, item, that._tagName, function (err, result) {
-                    if (err) {
-                        return deferred.reject(err);
-                    }
-                    else if (result) {
-                        return deferred.resolve(result);
-                    } else {
-                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
-                    }
-                });
-                return deferred.promise;
-            };
-            DB.prototype.save = function (item) {
-                var that = this;
-                var deferred = $q.defer();
-                if (typeof item == 'undefined') {
-                    return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
-                }
-                Buildfire.datastore.save(item, that._tagName, function (err, result) {
-                    if (err) {
-                        return deferred.reject(err);
-                    }
-                    else if (result) {
-                        return deferred.resolve(result);
-                    } else {
-                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
-                    }
-                });
-                return deferred.promise;
-            };
-            DB.prototype.delete = function (id) {
-                var that = this;
-                var deferred = $q.defer();
-                if (typeof id == 'undefined') {
-                    return deferred.reject(new Error(MESSAGES.ERROR.ID_NOT_DEFINED));
-                }
-                Buildfire.datastore.delete(id, that._tagName, function (err, result) {
-                    if (err) {
-                        return deferred.reject(err);
-                    }
-                    else if (result) {
-                        return deferred.resolve(result);
-                    } else {
-                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
                     }
                 });
                 return deferred.promise;
