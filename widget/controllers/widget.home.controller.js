d2f2cde53f657633c77fe1a05176721486a0e8d7
vineetasharma <vineetasharma18791@gmail.com>
2016-02-04 14:31:52 +0530

    Multiple notifications of a successful removal of a track in playlist is shown if the user removes a song from the playlist : Fixed

--- widget/controllers/widget.home.controller.js
+++ widget/controllers/widget.home.controller.js
@@ -75,17 +75,17 @@
                 );
 
                 WidgetHome.goToTrack = function (track) {
-                    WidgetHome.showTrackSlider=false;
+                    WidgetHome.showTrackSlider = false;
                     console.log('Goto Track called---------------------------------------', track);
                     audioPlayer.pause();
-                    $timeout(function(){
+                    $timeout(function () {
                         WidgetHome.playTrack();
-                    },1000);
+                    }, 1000);
                     $rootScope.playTrack = true;
                     WidgetHome.currentTime = null;
                     WidgetHome.duration = null;
                     WidgetHome.currentTrack = track;
-                    console.log('In track------------------------WidgetHome.currentTime',WidgetHome.currentTime,'WidgetHome.duration========',WidgetHome.duration);
+                    console.log('In track------------------------WidgetHome.currentTime', WidgetHome.currentTime, 'WidgetHome.duration========', WidgetHome.duration);
                     console.log('Goto Track called---------------$rootScope playTrack------------------------', $rootScope.playTrack);
                     if (!$rootScope.$$phase)$rootScope.$digest();
                 };
@@ -97,7 +97,7 @@
                     console.log('WidgetHome.page', WidgetHome.page);
                     WidgetHome.isBusy = true;
                     if (WidgetHome.info && WidgetHome.info.data && WidgetHome.info.data.content && WidgetHome.info.data.content.link)
-                        soundCloudAPI.getTracks(WidgetHome.info.data.content.link, ++WidgetHome.page,WidgetHome.pageSize)
+                        soundCloudAPI.getTracks(WidgetHome.info.data.content.link, ++WidgetHome.page, WidgetHome.pageSize)
                             .then(function (data) {
                                 WidgetHome.noTracks = false;
                                 console.log('Got tracks--------------------------', data);
@@ -157,18 +157,21 @@
                  * Player related method and variables
                  */
                 WidgetHome.playTrack = function () {
-                    WidgetHome.showTrackSlider=true;
+                    WidgetHome.showTrackSlider = true;
                     console.log('Widget HOme url----------------------', WidgetHome.currentTrack.stream_url + '?client_id=' + WidgetHome.info.data.content.soundcloudClientID);
                     WidgetHome.playing = true;
                     if (WidgetHome.paused) {
                         audioPlayer.play();
                     } else {
-                        audioPlayer.play({url: WidgetHome.currentTrack.stream_url + '?client_id=' + WidgetHome.info.data.content.soundcloudClientID, title:WidgetHome.currentTrack.title});
+                        audioPlayer.play({
+                            url: WidgetHome.currentTrack.stream_url + '?client_id=' + WidgetHome.info.data.content.soundcloudClientID,
+                            title: WidgetHome.currentTrack.title
+                        });
                     }
                 };
                 WidgetHome.playlistPlay = function (track) {
-                    WidgetHome.showTrackSlider=true;
-                    WidgetHome.currentTrack=track;
+                    WidgetHome.showTrackSlider = true;
+                    WidgetHome.currentTrack = track;
                     console.log('PlayList Play ---------------Track is played', track);
                     WidgetHome.playing = true;
                     if (track) {
@@ -241,10 +244,12 @@
                     var playListTrack = new Track(track.title, track.stream_url + '?client_id=' + WidgetHome.info.data.content.soundcloudClientID, track.artwork_url, track.tag_list, track.user.username);
                     console.log('removeFromPlaylist called-------------------------------');
                     if (WidgetHome.playList) {
-                        var trackIndex;
+                        var trackIndex = 0;
                         WidgetHome.playList.filter(function (val, index) {
-                            if (val.url == track.stream_url + '?client_id=' + WidgetHome.info.data.content.soundcloudClientID)
+                            if (((val.url == track.stream_url + '?client_id=' + WidgetHome.info.data.content.soundcloudClientID) || val.url == track.url) && (trackIndex == 0)) {
                                 audioPlayer.removeFromPlaylist(index);
+                                trackIndex++;
+                            }
                             return index;
 
                         });
@@ -332,7 +337,7 @@
                     WidgetHome.paused = false;
                     WidgetHome.currentTrack = null;
                     WidgetHome.duration = '';
-                    WidgetHome.showTrackSlider=false;
+                    WidgetHome.showTrackSlider = false;
                 });
 
                 /**
