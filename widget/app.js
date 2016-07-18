dd2554308b48033f2cd5f07330cdcf35aecb7f3e
tabish.khan <tabish.khan@tothenew.com>
2016-07-18 20:47:15 +0530

    Added important class for the background image

--- widget/app.js
+++ widget/app.js
@@ -110,7 +110,7 @@
                   element.attr("style", 'background-color:white');
                 }
                 element.css({
-                  'background-size': 'cover'
+                  'background-size': 'cover !important'
                 });
               });
               // img = $filter("cropImage")(value, $rootScope.deviceWidth, $rootScope.deviceHeight, true);
